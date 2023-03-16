892c5496951e3d7a9ce28ec9d4a4dc3ec046e2b5
wairimu92 <wwainaina94@gmail.com>
2023-03-16 08:00:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h>
+/**
+ * main - printing a line with the puts function
+ * return 0
+ */
+int main(void)
+	{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
